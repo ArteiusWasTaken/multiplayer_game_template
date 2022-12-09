211648d827bd4bace82f838cfb64514ac7c0f9a0
ArteiusWasTaken <sebastiancifer@gmail.com>
2022-12-09 10:51:28 -0600

    eee

--- lib/resources/socket_client.dart
+++ lib/resources/socket_client.dart
@@ -1,14 +1,15 @@
-import 'package:socket_io_client/socket_io_client.dart' as sio;
+import 'package:socket_io_client/socket_io_client.dart' as IO;
 
 class SocketClient {
-  sio.Socket? socket;
+  IO.Socket? socket;
   static SocketClient? _instance;
 
   SocketClient._internal() {
-    socket = sio.io('https://mpserver365.vercel.app', <String, dynamic>{
-      'transports': ['websocket'],
-      'autoConnect': false,
-    });
+    socket = IO.io(
+        'wss://mpserver365.vercel.app/socket.io/?EIO=3&transport=websocket',
+        <String, dynamic>{
+          'transports': ['websocket'],
+        });
     socket!.connect();
   }
 
